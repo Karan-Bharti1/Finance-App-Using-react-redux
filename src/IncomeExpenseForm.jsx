14c94326f805af182a06315e242c6c2545ae882c
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-04 16:56:43 +0530

    feat:created Income form and ,created and provided store as Provider context to app

---
+++ src/IncomeExpenseForm.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const IncomeExpenseForm=()=>{
+    const [amount,setAmount]=useState(0)
+    return (
+        <div>
+            <input  type="number" onChange={(e)=>setAmount(e.target.value)} value={amount}/>
+            <button onClick={handleAddIncome}>Add Income</button>
+            <button onClick={handleAddExpense}>Add Expense</button>
+        </div>
+    )
+}
+export default IncomeExpenseForm;
---
+++ src/store.jsx
@@ -0,0 +1,5 @@
+import { createStore } from "redux";
+const financeReducer=(state,action)=>{
+    
+}
+export default createStore(financeReducer)
